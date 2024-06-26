bb175108e5bcc4abcd5bb1590c958491a806cbe3
gaurang-agileinfoways <gaurang.patel@agileinfoways.com>
2024-06-26 17:53:04 +0530

    leaderboard

--- src/page/deshboard/index.tsx
+++ src/page/deshboard/index.tsx
@@ -52,27 +52,31 @@ export const Deshboard = () => {
               </svg>
               Start The Test
             </Link>
-            <a
-              href="sdfd"
-              rel="noopener"
-              target="_blank"
+            <Link
+              to={ROUTES.leaderboard}
               className="rounded-full text-center transition px-5 py-2.5 bg-white border-2 border-indigo-700 hover:bg-indigo-50 text-indigo-800 flex gap-1 items-center justify-center"
             >
               <svg
-                fill="none"
+                width="24"
+                height="24"
                 viewBox="0 0 24 24"
-                strokeWidth={2}
-                className="text-indigo-700 w-5 h-5 mr-2"
+                fill="none"
                 stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-                />
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M8 21l8 0" />
+                <path d="M12 17l0 4" />
+                <path d="M7 4l10 0" />
+                <path d="M17 4v8a5 5 0 0 1 -10 0v-8" />
+                <path d="M5 9m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                <path d="M19 9m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
               </svg>
-              Top palyers
-            </a>
+              Leaderboard
+            </Link>
           </div>
         </div>
       </div>
